8404593397535ff1103f8d25e091512ac83dc7dc
regnick95 <rsyln001@fiu.edu>
2020-05-17 17:26:05 -0500

    added print statements for degree inputs

--- Main.c
+++ Main.c
@@ -6,6 +6,12 @@ int main()
     int n;
     int num1 = 35;
     int num2 = 45;
+    int c_degree = 28;
+    int f_degree = 85;
+    
+    printf("%d° celcius is equal to %d° fahrenheit\n", c_degree, cel_to_far(c_degree));
+    printf("%d° fahrenheit is equal to %d° celcius\n", f_degree, far_to_cel(f_degree));
+  
     
     n = Fib(9);
     printf("Fibonacci of 9 is : %d\n", n);
