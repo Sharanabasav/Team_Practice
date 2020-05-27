c33c9bdfe8ecd9a5a42c99526ea2d9e451b13bc5
Sharan <sharanm087@gmail.com>
2020-05-26 19:56:11 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "Common.h"
 
+/* Call Pointer to function to double the Salary */
+double *multiplyByTwo (double *input) {
+  
+  /* Memory allocation in Heap */
+  double *twice = malloc(sizeof(double));
+  *twice = *input * 2.0;
+  return twice;
+}
+
 int main()
   {
     int n;
@@ -22,6 +31,28 @@ int main()
     /*calling swap function*/
     swapnum(&num1, &num2);
     printf("\nAfter swapping: %d, %d", num1, num2);
+  
+    /* Memory allocation in Heap */
+  int *age = malloc(sizeof(int));
+  *age = 30;
+  double *salary = malloc(sizeof(double));
+  *salary = 12345.67;
+  double *myList = malloc(3 * sizeof(double));
+  myList[0] = 1.2;
+  myList[1] = 2.3;
+  myList[2] = 3.4;
+
+  double *twiceSalary = multiplyByTwo(salary);
+
+  printf("double your salary is %.3f\n", *twiceSalary);
+
+  /* Releasing Memory allocation from Heap */
+  free(age);
+  free(salary);
+  free(myList);
+  free(twiceSalary);
+  
+    return 0;
  
   }
   
